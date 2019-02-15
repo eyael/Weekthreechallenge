cb2f704f0667384b87234d82bba4ac3bee4767fb
eyael <47112888+eyael@users.noreply.github.com>
2019-02-15 13:02:12 -0500

    Update Item.java

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,13 +1,39 @@
-package com.company;
+import java.util.ArrayList;
 
-public class Item  {
+public class address {
+    String street;
+    int zipcode;
+    String state;
+    public address(String street, int zipcode, String state) {
+        this.street = street;
+        this.zipcode = zipcode;
+        this.state = state;
+    }
 
+    public String getStreet() {
+        return street;
+    }
 
-    public Item() {
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
-
-
 
 
 
